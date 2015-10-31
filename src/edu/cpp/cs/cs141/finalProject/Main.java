654ccee38821993c5b3a123972d88d7e863d4a13
Khanh Nguyen <baokhanh_n@hotmail.com>
2015-10-30 22:29:01 -0700

    First commit

---
+++ src/edu/cpp/cs/cs141/finalProject/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cpp.cs.cs141.finalProject;
+
+/**
+ * @author Khanh
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
