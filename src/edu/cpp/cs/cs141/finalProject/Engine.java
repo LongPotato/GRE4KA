2f56acfc15a3ac98168fe6986a160d1a950429b8
Kevin De La Torre <kevindelatorre@live.com>
2015-11-06 13:08:15 -0800

    Khanh doesn't trust me

--- src/edu/cpp/cs/cs141/finalProject/Engine.java
+++ src/edu/cpp/cs/cs141/finalProject/Engine.java
@@ -26,11 +26,12 @@ public class Engine {
 	private Square[][] map = new Square[9][9];
 	
 	/**
-	 * Initialize/fill up the 2 dimentional array map with Square objects.
-	 * Asign the row & col attributes for each Square.
+	 * Initialize/fill up the 2 dimensional array map with Square objects.
+	 * Assign the row & col attributes for each Square.
 	 */
-	public void fillMap() {
+	public void fillMapWithSquare() {
 		Square[][] map = this.map;
+			
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
 				Square location = new Square(i, j);
@@ -43,9 +44,42 @@ public class Engine {
      * Set up the map with 1 spy, 6 ninjas, and 9 rooms.
      */
     public void setUpMap() {
+    	assignRooms();
+    	assignSpy();
     	// We can do that all in one, or divide it into sub methods
     }
+
+    public void assignRooms() {
+    	 
+    	
+    	Room location1 = new Room( 1, 1) ;
+    	Room location2 = new Room( 1, 4) ;
+    	Room location3 = new Room( 1, 7) ;
+    	Room location4 = new Room( 4, 1) ;
+    	Room location6 = new Room( 4, 4) ;
+    	Room location7 = new Room( 4, 7) ;
+    	Room location5 = new Room( 7, 1) ;
+    	Room location8 = new Room( 7, 4) ;
+    	Room location9 = new Room( 7, 7) ; 
+    	
+    	map[1][1] = location1;
+    	map[1][4] = location2;
+    	map[1][7] = location3;
+    	map[4][1] = location4;
+    	map[4][4] = location5;
+    	map[4][7] = location6;
+    	map[7][1] = location7;
+    	map[7][4] = location8;
+    	map[7][7] = location9;
+   
+    }
     
+    public void assignSpy() {
+    	Spy location = new Spy (8,0);
+    	map [8][0] = location;
+    }
+    
+
 	/**
 	 * Return the game map.
 	 * @return a 2 dimensional array of type Square.
@@ -117,5 +151,25 @@ public class Engine {
 		return true;
 	}
 	
-	
+    
+	/**
+	    
+	    public void assignSpyAndVisibility(){
+	    	Square spyLocation = new Square("S", 0, 0);
+	    	x = spyLocation.setRow(0);
+	    	y = spyLocation.setCol(8);
+	    	
+	    	for (int i = 0; i < map.length; ++i) {
+	    		for (int j = 0; j < map[i].length; ++j) {
+	    			if (i = y && abs(x - j) < 2){
+	    				
+	    			}
+	    			if (map[i][j].getSymbol().equals("X"))
+	    				map[i][j].setSymbol(" ");
+	    			else 
+	    				map[i][j].getSymbol();
+	    			}
+	    		}
+	    	}
+	    */
 }
